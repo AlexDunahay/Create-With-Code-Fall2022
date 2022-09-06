326af311a3a42e9ed7a37a750fd8094789ce195a
AlexDunahay <25dunaa@elida.k12.oh.us>
2022-09-06 12:20:19 -0400

    Finished Prototype 1 - Car
    Can turn and go forward/backward off input buttons

--- Prototype 1 - Car/Assets/Scripts/PlayerController.cs
+++ Prototype 1 - Car/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerController: MonoBehaviour
 {
-    public float speed = 20;
-    public float turnSpeed;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +18,12 @@ public class PlayerController: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Moves car based of vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates car based off horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
